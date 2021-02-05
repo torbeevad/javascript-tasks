174afabd8ad83bdc7373cd5bdf9857474d95e5e1
Alexey Torbeev <torbec@yandex.ru>
2021-02-05 13:21:00 +0300

    qwe

--- projects/dom/index.js
+++ projects/dom/index.js
@@ -189,7 +189,22 @@ function collectDOMStat(root) {
      elements: [div]
    }
  */
-function observeChildNodes(where, fn) {}
+function observeChildNodes(where, fn) {
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (mutation.type === 'childList') {
+        fn({
+          type: mutation.addedNodes.length ? 'insert' : 'remove',
+          nodes: [
+            ...(mutation.addedNodes.length ? mutation.addedNodes : mutation.removedNodes),
+          ],
+        });
+      }
+    });
+  });
+
+  observer.observe(where, { childList: true, subtree: true });
+}
 
 export {
   createDivWithText,
